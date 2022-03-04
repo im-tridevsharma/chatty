90418e06e18a90e5ed18ea1d0f1a395945c0eaed
Tridev Sharma <sendmailtotridev@gmail.com>
2022-03-04 21:29:08 +0530

    number popup

--- src/screens/GalleryViewScreen.js
+++ src/screens/GalleryViewScreen.js
@@ -8,17 +8,21 @@ import {
   Image,
   Dimensions,
   TouchableWithoutFeedback,
+  Text,
 } from "react-native";
 import style from "../config/style";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import CameraRoll from "@react-native-community/cameraroll";
 import ImagePreview from "../components/ImagePreview";
 import { useMainContext } from "../context";
+import OutlineButton from "../components/OutlineButton";
 
 const GalleryViewScreen = ({ navigation }) => {
   const { theme, scheme } = useMainContext();
   const [photos, setPhotos] = React.useState([]);
   const [preview, setPreview] = React.useState(false);
+  const [noPermission, setNoPermission] = React.useState(false);
+  const [askAgain, setAskAgain] = React.useState(0);
   const { width } = Dimensions.get("window");
 
   const hasAndroidPermission = async () => {
@@ -37,6 +41,7 @@ const GalleryViewScreen = ({ navigation }) => {
     (async function () {
       const res = await hasAndroidPermission();
       if (res) {
+        setNoPermission(false);
         CameraRoll.getPhotos({
           first: 100,
           assetType: "Photos",
@@ -45,9 +50,11 @@ const GalleryViewScreen = ({ navigation }) => {
             setPhotos(r.edges);
           })
           .catch((err) => console.log(err));
+      } else {
+        setNoPermission(true);
       }
     })();
-  }, []);
+  }, [askAgain]);
 
   return (
     <View
@@ -69,29 +76,55 @@ const GalleryViewScreen = ({ navigation }) => {
         </TouchableOpacity>
       </View>
 
+      {noPermission && (
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: scheme === "light" ? theme.gray : theme.white,
+              fontFamily: "Montserrat-Bold",
+              textAlign: "center",
+            }}
+          >
+            Please allow permission to select images from your gallery.
+          </Text>
+          <OutlineButton
+            label="Allow"
+            onPress={() => setAskAgain(Date.now())}
+          />
+        </View>
+      )}
+
       {preview && (
         <View style={{ ...StyleSheet.absoluteFill, zIndex: 100 }}>
           <ImagePreview source={preview} setPreview={setPreview} />
         </View>
       )}
 
-      <FlatList
-        data={photos}
-        numColumns={3}
-        keyExtractor={(item) => item.node.modified}
-        renderItem={({ item }) => (
-          <TouchableWithoutFeedback
-            onPress={() => setPreview(item.node.image.uri)}
-            style={{ flex: 1 }}
-          >
-            <Image
-              source={{ uri: item.node.image.uri }}
-              style={{ width: width / 3, height: 150 }}
-              resizeMode="cover"
-            />
-          </TouchableWithoutFeedback>
-        )}
-      />
+      {photos?.length > 0 && (
+        <FlatList
+          data={photos}
+          numColumns={3}
+          keyExtractor={(item) => item.node.modified}
+          renderItem={({ item }) => (
+            <TouchableWithoutFeedback
+              onPress={() => setPreview(item.node.image.uri)}
+              style={{ flex: 1 }}
+            >
+              <Image
+                source={{ uri: item.node.image.uri }}
+                style={{ width: width / 3, height: 150 }}
+                resizeMode="cover"
+              />
+            </TouchableWithoutFeedback>
+          )}
+        />
+      )}
     </View>
   );
 };
